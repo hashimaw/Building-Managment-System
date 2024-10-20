9a28cad131edd48192d8a7f9dd7bef3ee9a53bf8
Hashim <hashimabdrehman@gmail.com>
2024-10-20 16:28:34 +0300

    rollback to maria and removing array selections that were for my sql

--- Backend/database.js
+++ Backend/database.js
@@ -1,13 +1,13 @@
-import mysql from "mysql2"
+import mariadb from "mariadb"
 import dotenv from "dotenv"
 dotenv.config();
 
-var pool = mysql.createPool({
+var pool = mariadb.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
-}).promise()
+});
 
 
 export default pool;
